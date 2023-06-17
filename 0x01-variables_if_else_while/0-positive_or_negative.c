a78e84f3e7a1547fe877c1aab9f2dce9be789746
“Maha <mahakamal09@gmail.com>
2023-06-16 18:02:44 -0700

    positive number commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print whether the number stored in the variable n is positive or negative
+ *
+ * Return: Alwayes 0 success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+	{
+		printf("%dis positive\n,n");
+	}
+	else if (n==0)
+	{
+		printf("%dis zero\n,n");
+	}
+	else
+	{
+		printf("%d is negative\n,n")
+	}
+	return (0);
+}
