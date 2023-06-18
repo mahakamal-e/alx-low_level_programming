380aa3ffb7cf35dc8586802a6993ad7da3100eb0
“Maha <mahakamal09@gmail.com>
2023-06-18 15:33:42 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: alwayes 0 success
+*/
+
+int main(void)
+{
+	char lower = 'z';
+	while (lower >= 'a')
+	{
+		putchar(lower);
+		lower--;
+	}
+	putchar('\n');
+
+	return (0);
+}
