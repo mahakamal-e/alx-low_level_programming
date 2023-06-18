0a6f866996cc1df9dc745ea0f4a0722ff88c8916
“Maha <mahakamal09@gmail.com>
2023-06-18 14:56:32 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase,
+ *
+ * Return: Alwayes 0
+*/
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		if (lower == 'e' || lower == 'q')
+			lower++;
+		putchar(lower);
+		lower++;
+	}
+	putchar('\n');
+
+	return (0);
