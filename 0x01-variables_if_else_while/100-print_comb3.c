6b292d1f7958962a252012478b87a9645d9474f8
“Maha <mahakamal09@gmail.com>
2023-06-19 17:50:54 -0700

    dond

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: Alwayes 0
+*/
+int mai(void)
+{
+	int f_number = 0, s_number;
+
+	while (f_number <= 99)
+	{
+		s_number = f_number;
+		while (s_number <= 99)
+		{
+			if (s_number != f_number)
+			{
+				putchar((f_number / 10) + 48);
+				putchar((f_number % 10) + 48);
+				putchar(' ');
+				putchar((s_number / 10) + 48);
+				putchr((s_number % 10) + 48);
+
+				if (f_number != 98 || s_number != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			s_number++;
+		}
+		f_number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
