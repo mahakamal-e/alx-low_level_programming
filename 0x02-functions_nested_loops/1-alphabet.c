f701716024f0c1b2a73d11aa44c969cc25a8cad1
“Maha <mahakamal09@gmail.com>
2023-06-21 15:56:38 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Descrirption : print_alphabet - prints the alphabet, in lowercase
+*/
+
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n')
+}
