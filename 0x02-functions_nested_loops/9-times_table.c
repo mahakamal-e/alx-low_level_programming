4ee24358dc1ea88efe6a30063f3815d517f50f70
“Maha <mahakamal09@gmail.com>
2023-06-21 17:57:53 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,8 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (multiply = 0; multiply <= 9; multiply++)
+		_putchar('0');
+		for (multiply = 1; multiply <= 9; multiply++)
 		{
 			_putchar(',');
 			_putchar(' ');
