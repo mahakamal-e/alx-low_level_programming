316fe15c3c4804d9015c1a66e3daa8d7bb3873b6
“Maha <mahakamal09@gmail.com>
2023-06-22 15:00:34 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,12 +5,19 @@
 */
 void print_square(int size)
 {
-	int i, j;
-
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= size; j++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
 			_putchar('#');
 		_putchar('\n');
+		}
 	}
 }
