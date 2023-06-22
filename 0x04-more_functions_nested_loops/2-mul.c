3a6ed3c565928ff9fceae8cb4b9276417a3ee2ee
“Maha <mahakamal09@gmail.com>
2023-06-22 12:20:40 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers.
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return result of multipy
+*/
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
