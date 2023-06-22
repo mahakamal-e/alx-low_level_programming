5e4144727e03deb4f0b64e69067391cd0370e133
“Maha <mahakamal09@gmail.com>
2023-06-21 17:03:49 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,10 @@ int print_last_digit(int n)
 	int lastdigit;
 
 	if (n < 0)
-		lastdigit = n % 10;
+	{
+		n = n * -1;
+		lastdigit = (n % 10);
+	}
 	else
 		lastdigit = n % 10;
 	_putchar(lastdigit + '0');
