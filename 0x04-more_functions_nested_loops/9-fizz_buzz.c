02f48e2b12b452c88ac4f6508f97345ae504f6fc
“Maha <mahakamal09@gmail.com>
2023-06-22 15:15:38 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: print numbers from 1 to 100
+ *
+ * Return: alwayes 0 success
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 5 == 0 && num % 3 == 0)
+			printf("fizzBuzz");
+		else
+			printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
+
