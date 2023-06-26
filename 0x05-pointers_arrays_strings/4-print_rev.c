c6396287659d8cbeafca725f57086ff3e3959936
“Maha <mahakamal09@gmail.com>
2023-06-26 16:27:38 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line.
+ *
+ * @s: input of string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	while (i--)
+	{
+		_putchar(s[length]);
+	}
+
+	_putchar('\n');
+}
