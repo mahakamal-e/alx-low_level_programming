b6e8a478b00edaa13a3e7a4c29e65b797e3013c0
“Maha <mahakamal09@gmail.com>
2023-06-26 16:42:53 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: input of string
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int count = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (i < lenght)
+	{
+		lenght--;
+		temp = s[i];
+		s[i] = s[lenght];
+		s[length] = remp;
+		i++;
+	}
+}
