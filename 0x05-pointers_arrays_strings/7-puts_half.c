bd78c52e75fa85fe7b743b06a039490c906ec308
“Maha <mahakamal09@gmail.com>
2023-06-26 17:25:28 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,11 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; ++i)
 		;
 	i++;
 
-	for (i = length / 2; i < length; i++)
+	for (i = i / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
