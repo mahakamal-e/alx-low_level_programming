e6909491f9f644e6a6091502819a66985dc6c570
“Maha <mahakamal09@gmail.com>
2023-06-30 07:09:15 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - This function appends the src string to the dest string
+ * @stc: input value
+ * @dest: input value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int s1, s2;
+
+	for (s1 = 0; s1 < des[s1]; s1++);
+	
+	for (s2 = 0; s2 < src[s2]; s2++)
+	{
+		dest[s1++] = src[s2];
+	}
+	return (dest);
+}
