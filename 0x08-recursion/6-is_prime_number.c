29df7243240925f3d5f8aa5f865e56d8830e360d
“Maha <mahakamal09@gmail.com>
2023-07-05 09:23:58 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,14 +2,14 @@
 /**
  * is_prime_number - function return 1 if prime number 0otherwise
  * @n: input number
- * Return: 1 if prime 
+ * Return: 1 if prime
  * otherwise 0
  */
 int is_prime_number(int n)
 {
 	if (n == 1 || n <= 0)
 		return (0);
-	if (n % n == 0 && n > 0)
-		return (0);
+	else if (n % n == 0 && n > 0)
+		return (1);
 	return (is_prime_number(n));
 }
