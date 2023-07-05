38f5b882cb1e46f2646216edbd051152a552b1c5
“Maha <mahakamal09@gmail.com>
2023-07-05 09:00:15 -0700

    m

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,9 +7,9 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1)
-		return (1);
-	if ( n % n == 0 &&n > 0)
+	if (n == 1 || n <= 0)
 		return (0);
-	return (is_prime_number(n, n - 1));
+	if ( n % n == 0 && n > 0)
+		return (0);
+	return (is_prime_number(n)
 }
