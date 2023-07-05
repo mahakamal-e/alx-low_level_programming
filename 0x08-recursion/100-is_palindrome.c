832472f2156f861332ffc4433fa27e98dc8dafc9
“Maha <mahakamal09@gmail.com>
2023-07-05 13:57:43 -0700

    commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_palindrome - function return 1 if string empty
+ * @s: input string
+ * Return: 1 if string empty
+ * otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return(1);
+}
