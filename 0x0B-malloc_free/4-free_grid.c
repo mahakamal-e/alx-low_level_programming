88c4a0133cad9f7ed0dc789f669909fd54adda69
“Maha <mahakamal09@gmail.com>
2023-07-11 15:17:55 -0700

    N

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - a function that frees a 2 dimensional grid
+ * @grid: second pointer grid
+ * @height: height
+ */
+void free_grid(int **grid, int height)
+{
+	int iterat;
+
+	for (iterat = 0; iterat < height; iterat++)
+	{
+		free(grid[iterat]);
+	}
+	free(grid);
+}
