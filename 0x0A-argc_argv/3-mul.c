fb396fa34facadf47e37fa646da6281b28195985
“Maha <mahakamal09@gmail.com>
2023-10-02 16:32:54 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0 if success otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int multi;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multi);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
