3bddb7653c9047d9da0e094cd22d246d64a79bde
“Maha <mahakamal09@gmail.com>
2023-10-02 12:53:46 -0700

    libmy library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @haystack: first input
+ * @needle: second input
+ * Return: return 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
