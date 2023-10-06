e5490ac020b2bbed02ee28a70d916372d7ff43dc
“Maha <mahakamal09@gmail.com>
2023-10-06 15:29:48 -0700

    ddone

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * _calloc - a function that allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size bytes each element
+ * Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int totalSize;
+
+	totalSize = nmemb * size;
+
+	void *p = malloc(totalSize);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	if (p == NULL)
+		return (NULL);
+
+	memset(p, 0, totalSize);
+	return (p);
+}
