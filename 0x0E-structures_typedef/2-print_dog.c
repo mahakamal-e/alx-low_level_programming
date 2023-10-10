cac23171f9dff6f10cbb51eba8d223e0b47393c3
“Maha <mahakamal09@gmail.com>
2023-10-10 15:43:04 -0700

    done

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,10 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if ((*d).name == NULL)
+		if (d->name == NULL)
 			d->name = "(nil)";
-		if ((*d).owner == NULL)
+		if (d->owner == NULL)
 			d->owner = "(nil)";
 		printf("Name: %s\n,Age: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
-	else
-		return;
 }
