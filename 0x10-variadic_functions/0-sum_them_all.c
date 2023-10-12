fd42baf1ee0b31f8c084002bdd682897aa032177
“Maha <mahakamal09@gmail.com>
2023-10-12 05:32:13 -0700

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all parameters
+ *
+ * @n: number of paramters
+ * @...: paramters to calculate sum for
+ *
+ * Return: the sum
+ * If n == 0, return 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	int sum;
+	int iterat;
+
+	if (n <= 0)
+		return (0);
+	va_start(ptr, n);
+
+	sum = 0;
+	for (iterat = 0; iterat < n; iterat++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
