a74643fe1404acb187c857a069793356276579f6
mahakamal-e <mahakamal09@gmail.com>
2023-10-23 10:42:59 -0700

    git sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - return the sum of all data
+ * @head: pointer to first element
+ * Return: if list empty return 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum_ = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
