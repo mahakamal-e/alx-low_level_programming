a54359311ccdfecaf96347eacf7f56d73f1ce748
mahakamal-e <mahakamal09@gmail.com>
2023-10-26 08:50:09 -0700

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,10 +10,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int exclusive_value = n ^ m;
 	unsigned int count_bit = 0;
 
-	while (exclusive_value)
+	while (exclusive_value != 0)
 	{
-		exclusive_value = exclusive_value & (exclusive_value - 1);
-		count_bit++;
+		if (exclusive_value & 1ul)
+			count_bit++;
+		exclusive_value = exclusive_value >> 1;
 	}
 	return (count_bit);
 }
