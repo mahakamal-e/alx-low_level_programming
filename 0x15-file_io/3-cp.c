05263e0b7c6cbea017d6feee018c2c1d0fce8b85
mahakamal-e <mahakamal09@gmail.com>
2023-10-30 09:35:12 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,30 +21,30 @@ void handle_error(int code_error, const char *error_mess)
  */
 int main(int argc, char **argv)
 {
-	int _file_from = 0;
-	int _file_to = 0;
+	int file_from = 0;
+	int file_to = 0;
 	ssize_t _bytes;
 	char buffer_[BUFF_SIZE];
 
 	if (argc != 3)
 		handle_error(97, "Usage: cp file_from file_to");
 
-	_file_from = open(argv[1], O_RDONLY);
-	if (_file_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 		handle_error(98, "Can't read from file");
 
-	_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-	if (_file_to == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	if (file_to == -1)
 		handle_error(99, "Can't write to file");
 
-	while ((_bytes = read(_file_from, buffer_, BUFF_SIZE)) > 0)
+	while ((_bytes = read(file_from, buffer_, BUFF_SIZE)) > 0)
 	{
-		if (write(_file_to, buffer_, _bytes) != _bytes)
+		if (write(file_to, buffer_, _bytes) != _bytes)
 			handle_error(99, "Can't write to file");
 	}
 	if (_bytes == -1)
 		handle_error(98, "Can't read from file");
-	if (close(_file_from) == -1 || close(_file_to) == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 		handle_error(100, "Can't close fd");
 	return (0);
 }
