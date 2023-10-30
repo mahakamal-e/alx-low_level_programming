dd6698c50b6dd1e323f5bed8fd3e368a3d3dee8c
mahakamal-e <mahakamal09@gmail.com>
2023-10-30 08:14:28 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,6 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t length = 0;
 	ssize_t i;
 	ssize_t _bytes = 0;
 
@@ -19,14 +18,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	for (i = 0 ; text_content[i] != '\0'; i++)
-		length++;
-
-	_bytes = write(fd, text_content, length);
-
-	if (_bytes == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0 ; text_content[i] != '\0'; i++)
+			;
+		_bytes = write(fd, text_content, i);
+		if (_bytes == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
