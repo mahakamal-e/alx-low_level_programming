e083e34ea5ff4f3e2aab8ffa1eae7148784c9429
mahakamal-e <mahakamal09@gmail.com>
2023-10-30 07:40:24 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,10 +25,9 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	_bytes = write(fd, text_content, i);
-	close(fd);
 
 	if (_bytes == -1)
 		return (-1);
-	else
-		return (1);
+	close(fd);
+	return (1);
 }
