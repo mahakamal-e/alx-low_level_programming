14eb07f837f1ed7fb73aa8f5011349cdb5b1ef48
mahakamal-e <mahakamal09@gmail.com>
2023-10-30 10:21:52 -0700

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,8 +9,8 @@
  */
 void handle_error(int code_error, const char *error_mess)
 {
-        dprintf(STDERR_FILENO, "Error: %s\n", error_mess);
-        exit(code_error);
+	dprintf(STDERR_FILENO, "Error: %s\n", error_mess);
+	exit(code_error);
 }
 
 /**
@@ -24,17 +24,15 @@ int main(int argc, char **argv)
 	int file_from, file_to;
 	ssize_t _bytes;
 	char buffer_[BUFF_SIZE];
-	char *_file_from = argv[1];
-	char *_file_to = argv[2];
 
 	if (argc != 3)
 		handle_error(97, "Usage: cp file_from file_to");
 
-	file_from = open(_file_from, O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 		handle_error(98, "Can't read from file");
 
-	file_to = open(_file_to, O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (file_to == -1)
 		handle_error(99, "Can't write to file");
 
