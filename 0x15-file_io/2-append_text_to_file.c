2d90d930d3466f2c74b7043150003bfcd5e3b3dc
mahakamal-e <mahakamal09@gmail.com>
2023-10-30 07:49:18 -0700

    commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,13 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		length++;
 
 	if (length)
-	{
 		_bytes = write(fd, text_content, length);
-	}
-	close(fd);
 
 	if (_bytes == length)
 		return (-1);
-	else
-		return (1);
+	close(fd);
+	return (1);
 }
