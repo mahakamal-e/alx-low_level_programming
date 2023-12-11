722e8ecabe41d1644f6de7e720ea603a46935e2d
mahakamal-e <mahakamal09@gmail.com>
2023-12-11 09:42:35 -0800

    :)

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p https://github.com/mahakamal-e/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/root/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
+wget -p /tmp https://github.com/mahakamal-e/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
