92a753626da08b8dee1ce07a88fe41b893e992ec
mahakamal-e <mahakamal09@gmail.com>
2023-12-21 18:40:11 +0000

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - a function that prints a hash table.
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp_node;
+	int printed_data = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp_node = ht->array[i];
+		while(temp_node != NULL)
+		{
+			if (printed_data == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp_node->key, temp_node->value);
+			printed_data = 1;
+			temp_node = temp_node->next;
+		}
+	}
+		printf("}\n");
+}
