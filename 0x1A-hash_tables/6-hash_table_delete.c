7ebd867b9098e2db555bfca8ad408e7e3afebd35
mahakamal-e <mahakamal09@gmail.com>
2023-12-21 19:00:50 +0000

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,15 +12,17 @@ void hash_table_delete(hash_table_t *ht)
 		return;
 	for (i = 0 ; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
 		while (ht->array[i] != NULL)
 		{
-			tmp = tmp->next;
+			tmp = ht->array[i]->next;
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
 			free(ht->array[i]);
+			ht->array[i] = tmp;
 		}
 	}
 	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
 	free(ht);
 }
